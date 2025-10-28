8bd0a7f5d93064761ba7cdbe7e136d6a56535936
grabbedbyalienspayments-eng <grabbedbyalienspayments@gmail.com>
2025-10-28 22:57:57 +0200

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'node:path'
+
+const base = process.env.BASE_PATH || '/'
+const isPreview = process.env.IS_PREVIEW ? true : false;
+
+// https://vite.dev/config/
+export default defineConfig({
+  define: {
+   __BASE_PATH__: JSON.stringify(base),
+   __IS_PREVIEW__: JSON.stringify(isPreview)
+  },
+  plugins: [react()],
+  base,
+  build: {
+    sourcemap: false,
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html')
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  }
+})
